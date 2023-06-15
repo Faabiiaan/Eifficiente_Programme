7f42e18c92cc62e4f40834ece1449976208ba0cf
FlorianRauber <35396217+FlorianRauber@users.noreply.github.com>
2023-06-15 19:57:28 +0200

    Create conway_8.c
    zaehllebende, findnachbarn und prueferegeln wurden in main integriert und etwas vereinfacht.

---
+++ conway_8.c
@@ -0,0 +1,219 @@
+#include <c64.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define P_SCREEN0 ((unsigned char *)0x2000)
+#define P_COLOR0  ((unsigned char *)0x0400)
+
+#define P_SCREEN1 ((unsigned char *)0x6000)
+#define P_COLOR1  ((unsigned char *)0x4400)
+
+//HighRes Mode 300x200px
+#define XMAX 40
+#define YMAX 25
+#define BOXSIZE 3
+#define ROUNDS 1
+
+void initSpielfeld(int spielfeld [][YMAX]);
+void printSpielfeld(int spielfeld [][YMAX]);
+
+
+//static const char array[XMAX][YMAX] 
+const static int array[XMAX][YMAX]= {
+{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1},
+{1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,1,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1},
+{0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+{1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+};
+
+static int spielfeld[XMAX][YMAX];
+static int temp[XMAX][YMAX];
+
+int main(void)
+{
+  clock_t       t;
+  unsigned long sec;
+  unsigned      sec10;
+  unsigned long fps;
+  unsigned      fps10;
+  unsigned char background;
+  unsigned char text;
+        
+	char x;
+	char y;
+	char xR;
+	char xL;
+	char yO;
+	char yU;
+	char lebende;
+	unsigned char round = 0;
+
+  t = clock ();
+	initSpielfeld(spielfeld);
+  clrscr();
+	background = bgcolor(COLOR_BLACK);
+	text = textcolor(COLOR_WHITE);
+	printSpielfeld(spielfeld);
+//	signal (int sig, __sigfunc func);
+
+
+	while(round < ROUNDS && !kbhit()){
+		for(y = 0; y< YMAX; y++){
+			for(x = 0; x< XMAX; x++){
+				yO = y + 1;
+				yU = y - 1;
+				xL = x - 1;
+				xR = x + 1;
+
+				if (x == 0) {
+					xL = XMAX - 1;
+				}
+				if (x == XMAX - 1) {
+					xR = 0;
+				}
+				if (y == 0) {
+					yO = YMAX - 1;
+				}
+				if (y == YMAX - 1) {
+					yU = 0;
+				}
+
+				gotoxy(0,0);
+				lebende = 0;
+				lebende += spielfeld[xL][yO];
+				lebende += spielfeld[x][yO];
+				lebende += spielfeld[xR][yO];
+				lebende += spielfeld[xL][y];
+				lebende += spielfeld[xR][y];
+				lebende += spielfeld[xL][yU];
+				lebende += spielfeld[x][yU];
+				lebende += spielfeld[xR][yU];
+
+
+				switch (lebende)
+				{
+				case 2: temp[x][y] = spielfeld[x][y];
+					break;
+
+				case 3: temp[x][y] = 1;
+					break;
+
+				default: temp[x][y] = 0;
+					break;
+				}
+				//findnachbarn
+				//lebende = zaehlLebende(nachbarn);
+				gotoxy(x,y);
+			}// for x
+		}// for y
+
+		memcpy(spielfeld,temp,XMAX*YMAX*2);
+	
+		round++;
+		printSpielfeld(spielfeld);	
+	}
+		t = clock() - t;
+	
+  /* Reset screen colors */
+    bgcolor (background);
+    textcolor (text);
+    clrscr ();
+
+    /* Calculate stats */
+    sec   = (t * 10) / CLK_TCK;
+    sec10 = sec % 10;
+    sec  /= 10;
+    fps   = (round * (CLK_TCK * 10)) / t;
+    fps10 = fps % 10;
+    fps  /= 10;
+
+    /* Output stats */
+    gotoxy (0, 0); cprintf ("time  : %lu.%us", sec, sec10);
+    gotoxy (0, 1); cprintf ("frames: %lu", round);
+    gotoxy (0, 2); cprintf ("fps   : %lu.%u", fps, fps10);
+
+    /* Wait for a key, then end */
+    cputsxy (0, 4, "Press any key when done...");
+    (void) cgetc ();
+
+    /* Done */
+    return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void printSpielfeld(int spielfeld [][YMAX]){
+	char x,y;
+	for(y = 0; y< YMAX; y++){
+		for(x = 0; x< XMAX; x++){
+			if(spielfeld[x][y] == 1){
+				revers(1);
+			} else{
+				revers(0);
+			}		
+ 			cputcxy (x, y, 32);
+		}
+	}
+}
+
+
+
+void initSpielfeld(int spielfeld [][YMAX]){
+	int x,y;
+	//fülle das feld mit zufallswerten und gibs aus
+	for(y = 0; y< YMAX; y++){
+		for(x = 0; x< XMAX; x++){
+				spielfeld[x][y] = array[x][y];
+		}
+	}
+}
